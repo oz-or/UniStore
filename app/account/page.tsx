5e2da82221151dc015af1ac675b56d9b3259b185
oz-or <fozordev@gmail.com>
2024-10-17 19:45:38 +0200

    Removed unneccesaary shadcn dependencies

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,8 +1,6 @@
 import { redirect } from "next/navigation";
 
 import { createClient } from "@/utils/supabase/server";
-import Navbar from "@/components/account/sidebar/Sidebar";
-import Footer from "@/components/account/Footer";
 
 export default async function PrivatePage() {
   //TODO: Display the first name, the last name the email and the address programmatically(in the placeholder of the Input field)
--- components/account/sidebar/Sidebar.tsx
+++ components/account/sidebar/Sidebar.tsx
@@ -1,8 +1,6 @@
 "use client";
-import { SidebarToggle } from "@/components/account/sidebar/SidebarToggle";
-import { Button } from "@/components/ui/button";
 
-import { cn } from "@/lib/utils";
+import { SidebarToggle } from "@/components/account/sidebar/SidebarToggle";
 import { PanelsTopLeft } from "lucide-react";
 import Link from "next/link";
 import { useState } from "react";
@@ -12,35 +10,29 @@ export function Sidebar() {
   const [isOpen, setIsOpen] = useState(true);
   return (
     <aside
-      className={cn(
-        "fixed top-0 left-0 z-20 h-screen translate-y-[138px] 1024:translate-y-[118px] lg:translate-x-0 transition-[width] ease-in-out duration-300 ",
-        !isOpen ? "w-[75px]" : "w-72"
-      )}
+      className={`fixed top-0 left-0 z-20 h-screen translate-y-[138px] 1024:translate-y-[118px] lg:translate-x-0 transition-[width] ease-in-out duration-300 
+        ${!isOpen ? "w-[75px]" : "w-72"}`}
     >
       <SidebarToggle isOpen={isOpen} setIsOpen={setIsOpen} />
       <div className="relative h-full flex flex-col px-3 py-4 overflow-y-auto shadow-md dark:shadow-zinc-800">
-        <Button
-          className={cn(
-            "transition-transform ease-in-out duration-300 mb-1",
-            !isOpen ? "translate-x-1" : "translate-x-0"
-          )}
-          variant="link"
-          asChild
-        >
+        <button className="inline-flex items-center justify-center gap-2 [&_svg]:size-5">
           <Link href="/account" className="flex items-center gap-2">
             <PanelsTopLeft className="w-6 h-6 mr-1" />
             <h1
-              className={cn(
-                "font-bold text-lg whitespace-nowrap transition-[transform,opacity,display] ease-in-out duration-300",
-                !isOpen
-                  ? "-translate-x-96 opacity-0 hidden"
-                  : "translate-x-0 opacity-100"
-              )}
+              className={`
+                font-bold text-lg whitespace-nowrap transition-[transform,opacity,display] ease-in-out duration-300
+                ${
+                  !isOpen
+                    ? "-translate-x-96 opacity-0 hidden"
+                    : "translate-x-0 opacity-100"
+                }
+              `}
             >
-              Brand
+              UniStore
             </h1>
           </Link>
-        </Button>
+        </button>
+
         <Menu isOpen={isOpen} />
       </div>
     </aside>
